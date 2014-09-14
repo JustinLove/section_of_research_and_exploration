ca5a2718c43145ffd8924cc4ed5905f3305e6c47
Justin Love <git@JustinLove.name>
2014-09-14 08:07:54 -0500

    seems to be working well

---
+++ ui/mods/section_of_research_and_exploration/section_of_research_and_exploration.js
@@ -0,0 +1,23 @@
+(function() {
+  var empty = {
+    summarize: function() {},
+    icon: function() {}
+  }
+  model.research_and_exploration = ko.observable(empty)
+  ko.computed(function() {
+    var card = model.selection.system().star.card()
+    if (card) {
+      new CardViewModel(card).card.then(model.research_and_exploration)
+    } else {
+      model.research_and_exploration(empty)
+    }
+  })
+
+  url = 'coui://ui/mods/section_of_research_and_exploration/section_of_research_and_exploration.html'
+  $.get(url, function(html) {
+    console.log("Loaded html " + url);
+    var $html = $(html)
+    $('#system-detail').append($html)
+    ko.applyBindings(model, $html[0])
+  })
+})()
